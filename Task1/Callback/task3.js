16f9f565900903e42a361eeca19adc57cd470989
Ghostx-x <nataligevorgyan2007@gmail.com>
2025-07-22 19:17:11 +0400

    task3

--- Task1/Callback/task3.js
+++ Task1/Callback/task3.js
@@ -19,21 +19,26 @@ function boggle(num1, num2, callback) {
     }, 2000)
 }
 
-function calculate(input, callback) {
-    fizzle(input, (qux) => {
-        function check(qux) {
-            if(qux<input) {
-                boggle(input, qux, (result) => {
-                    qux += result;
-                    check(qux)
-                })
-            } else {
-                callback(qux);
-            }
-        } check(qux);
-    })
-}
+// function calculate(input, callback) {
+//     fizzle(input, (qux) => {
+//         function check(qux) {
+//             if(qux<input) {
+//                 boggle(input, qux, (result) => {
+//                     qux += result;
+//                     check(qux)
+//                 })
+//             } else {
+//                 callback(qux);
+//             }
+//         } check(qux); 
+//     })
+// }
 
-calculate (10, (result) => {
-    return(result)
-})
+function calculate(input, callback) {
+    callback(input + 5);
+}
+// calculate (10, console.log)
+
+    calculate (10, (result) => {
+        return(result)
+    })
