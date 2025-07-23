75ca00fd05501063654ab221889b5b527f9982ca
Ghostx-x <nataligevorgyan2007@gmail.com>
2025-07-23 16:00:24 +0400

    new changes

--- Task2/functions.js
+++ Task2/functions.js
@@ -1,18 +1,17 @@
 async function fetchUsers() {
+    try{
     const userResponse = await fetch('https://jsonplaceholder.typicode.com/users/');
     const users = await userResponse.json();
-    // const userName = user.name;
-    // const userEmail = user.email;
 
     users.forEach(user => {
         console.log("User's name - ", user.name," User's email - ", user.email)
-    });
+    });}
+    
+    catch{
+        console.log("Error ", error);
+    }
     
 }
 
 export {fetchUsers};
 
-
-// const axios = require('axios');
-
-// axios.get('https://jsonplaceholder.typicode.com/users/')
--- Task2/index.js
+++ Task2/index.js
@@ -2,5 +2,3 @@ import {fetchUsers} from './functions.js'
 
 fetchUsers()
 
-
-// const {axiosData} = requier('./functions')
