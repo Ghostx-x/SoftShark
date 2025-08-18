825dcf17eda593b7d4c31391bbb0709cf22b8f24
Ghostx-x <nataligevorgyan2007@gmail.com>
2025-08-18 14:11:33 +0400

    docker

--- Task5/src/validations/validations.js
+++ Task5/src/validations/validations.js
@@ -3,7 +3,8 @@ const Joi = require('joi');
 const userSchema = Joi.object({
     name: Joi.string().alphanum().min(3).max(30),
     email: Joi.string().email(),
-    profession: Joi.string().alphanum()
+    profession: Joi.string().alphanum(),
+    metadata: Joi.object().optional()
 });
 
 module.exports = userSchema;
